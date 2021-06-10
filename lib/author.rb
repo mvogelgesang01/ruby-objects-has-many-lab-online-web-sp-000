db35d340c76c1dede83d17e964a5349820feadaa
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-06-10 20:38:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,11 +13,9 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
-    @@post_count += 1
     p = Post.new(name)
     self.add_post(p)
     p
